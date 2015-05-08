6ff1ac61ef958c876061a5b4263b7d8998ad7817
Daniel Chin <danielthank@gmail.com>
2015-05-08 12:47:16 +0800

    OK

--- pre_hw1.py
+++ pre_hw1.py
@@ -10,7 +10,7 @@ import theano.tensor as T
 import sys
 
 NUM_HIDDEN_UNITS = 512
-DATA = "train"
+DATA = "test"
 
 def build_model(input_dim, output_dim, num_hidden_units=NUM_HIDDEN_UNITS):
     l_in = lasagne.layers.InputLayer(
@@ -53,50 +53,20 @@ def build_model(input_dim, output_dim, num_hidden_units=NUM_HIDDEN_UNITS):
     )
     return l_out
 
-def printtrim(ans, fout):
-    for start in range(len(ans)):
-        if ans[start] != 37:
-            break
-    for end in range(len(ans)-1, -1, -1):
-        if ans[end] != 37:
-            break
-    pre = -1
-    for index in range(start, end+1):
-        if ans[index] == pre:
-            continue
-        print(util.index2ans[ans[index]], file=fout, end='')
-        pre = ans[index]
-    print(file=fout)
-
 def main():
     print("Loading data...")
     fbank = util.read_fbank(DATA)
-    label = util.read_label(DATA)
-    data = np.array([f[1:] for f in fbank]).astype(theano.config.floatX)
-    label = np.array([l[1] for l in label])
+    data = np.array([data[1:] for data in fbank]).astype(theano.config.floatX)
     output_layer = build_model(input_dim=69, output_dim=48)
-    lasagne.layers.set_all_param_values(output_layer, pickle.load(open("model/5d/66.97", "r")))
-    x = T.matrix('x', dtype=theano.config.floatX)
+    lasagne.layers.set_all_param_values(output_layer, pickle.load(open("model/5d/"+sys.argv[1], "r")))
+    x = T.matrix('x')
     predict = theano.function([x], T.argmax(output_layer.get_output(x, deterministic=True), axis=1))
-    """
-    fout = open(DATA + sys.argv[1]+".ans", "w")
-    print("id,phone_sequence", file=fout)
-    print("{}_{},".format(fbank[0][0][0], fbank[0][0][1]), file=fout, end='')
-    """
-    pre = 0
-    cnt = 0
+    fout = open(DATA + sys.argv[1]+".hw1.csv", "w")
+    print("Id,Prediction", file=fout)
+    ans = predict(data)
     for f, index in zip(fbank, range(len(fbank))):
-        if f[0][2] == 1 and index != 0:
-            ans = predict(data[pre:index])
-            for a, b in zip(ans, label[pre:index]):
-                if a == b:
-                    cnt += 1
-            pre = index
-    ans = predict(data[pre:])
-    for a, b in zip(ans, label[pre:]):
-        if a == b:
-            cnt += 1
-    print(float(cnt) / len(fbank))
+        data = np.array([f[1:]]).astype(theano.config.floatX)
+        print("{}_{}_{},{}".format(f[0][0], f[0][1], f[0][2], util.hw1[util.index2char[ans[index]]]), file=fout)
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import numpy as np
 index2char = {}
 char2index = {}
 index2ans = {}
+hw1 = {}
 a = 0
 chrmap = open('data/48_idx_chr.map','r')
 for line in chrmap:
@@ -9,6 +10,10 @@ for line in chrmap:
     index2char[int(line[1])] = line[0]
     char2index[line[0]] = int(line[1])
     index2ans[int(line[1])] = line[2]
+chrmap = open('data/48_39.map', "r")
+for line in chrmap:
+    line = line.split()
+    hw1[line[0]] = line[1]
 PHONES = 48
 FBANKS = 69
 
